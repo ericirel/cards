3a576ecae00cb8b3ba2afd17948e1608aa5c4927
eric-irel <eric.herskovic@gmail.com>
2014-10-23 12:30:30 -0400

    Clean up views, add project index

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -2,7 +2,7 @@ class CardsController < ApplicationController
 
   def create
     @project = Project.find(params[:project_id])
-    @card = Card.create(title: "new card", body: "write something here", project_id: @project_id)
+    @card = Card.create(title: "new card", body: "write something here", project_id: @project.id)
     redirect_to @project, notice: "new card added"
   end
 
